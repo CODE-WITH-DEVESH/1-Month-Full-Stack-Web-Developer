ee52cb7aba33408456c010f6cd03efce07bd275f
CODE-WITH-DEVESH <dksbaghel4@gmail.com>
2025-05-28 18:40:44 +0530

    summary

---
+++ Day 5/Summary.js
@@ -0,0 +1,19 @@
+// JavaScript has 8 Datatypes
+// String
+// Number
+// Bigint
+// Boolean
+// Undefined
+// Null
+// Symbol
+// Object
+
+// Non-Primitive Data Types
+
+//  Object Array  Function Date
+//  🧠 Important Points:
+// Non-primitive types are passed by reference, not by value.
+
+// Their values are mutable (can be changed).
+
+// All non-primitives are objects at their core (even arrays and functions).
