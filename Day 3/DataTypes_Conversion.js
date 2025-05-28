c1bdb5b1c673fd13b32e18d631ab6d18aee72149
CODE-WITH-DEVESH <dksbaghel4@gmail.com>
2025-05-28 17:58:51 +0530

    conversion

--- Day 3/DataTypes_Conversion.js
+++ Day 3/DataTypes_Conversion.js
@@ -14,3 +14,10 @@ console.log(typeof valueToNumber)
 let isloggedIn =1
 let BooleanIsLoggedIn = Boolean(isloggedIn)
 console.log(BooleanIsLoggedIn)
+
+
+let someNumber =23
+let stringtoNumber =String(someNumber)
+console.log(stringtoNumber);
+
+console.log(typeof stringtoNumber)
