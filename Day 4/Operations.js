61af36954c3a56ec59c18c057fa25f1426ef054a
CODE-WITH-DEVESH <dksbaghel4@gmail.com>
2025-05-28 18:15:20 +0530

    operations

---
+++ Day 4/Operations.js
@@ -0,0 +1,13 @@
+let str ="Devesh"
+let str1 =" Baghel"
+let str3 = str + str1
+console.log(str3);
+
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
