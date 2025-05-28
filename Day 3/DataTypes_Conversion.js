3b266bc39da294adcff0cf86dce6205546c93d7b
CODE-WITH-DEVESH <dksbaghel4@gmail.com>
2025-05-28 13:15:49 +0530

    DataTypes_Conversion

---
+++ Day 3/DataTypes_Conversion.js
@@ -0,0 +1,16 @@
+let score = "33"
+let score1 = null
+let score2 = undefined
+
+console.log(typeof score)
+console.log(typeof score1)
+console.log(typeof score2)
+
+
+let valueToNumber = Number(score)
+console.log(typeof valueToNumber)
+
+
+let isloggedIn =1
+let BooleanIsLoggedIn = Boolean(isloggedIn)
+console.log(BooleanIsLoggedIn)
